649aa94f72ce7d4e0f9c8234ea1e27b3d4607670
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-27 23:04:58 +0900

    画像表示用のCollectionViewを配置

---
+++ PixabayPhotoViewer/PixabayPhotoViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PixabayPhotoViewer
+//
+//  Created by home on 2019/07/27.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
