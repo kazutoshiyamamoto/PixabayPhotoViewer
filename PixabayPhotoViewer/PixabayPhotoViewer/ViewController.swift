1450aabfbcbabd735b2167735a5f78b8d46e2bc1
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-07-27 23:57:09 +0900

    pixabayの画像を表示する

--- PixabayPhotoViewer/PixabayPhotoViewer/ViewController.swift
+++ PixabayPhotoViewer/PixabayPhotoViewer/ViewController.swift
@@ -9,9 +9,20 @@
 import UIKit
 import Nuke
 
+struct Item: Codable {
+    var hits: [Hits]
+    struct Hits: Codable {
+        var user: String
+        var previewURL: String
+        var webformatURL: String
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var pixabayCollectionView: UICollectionView!
+    
+    private var items: Item?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +30,43 @@ class ViewController: UIViewController {
         
         self.pixabayCollectionView.delegate = self
         self.pixabayCollectionView.dataSource = self
+        
+        self.setUpCollectionItems()
+    }
+    
+    private func setUpCollectionItems() {
+        self.getCollectionItems(completionHandler: { (item) in
+            self.items = item
+            DispatchQueue.main.async {
+                self.pixabayCollectionView.reloadData()
+            }
+        })
+    }
+    
+    private func getCollectionItems(completionHandler: @escaping (Item) -> ()) {
+        let configuration = URLSessionConfiguration.default
+        
+        if let url = URL(string: "https://pixabay.com/api/?key={APIKey}&q=city&image_type=photo") {
+            self.getAddConfiguration(url: url, configuration: configuration, completionHandler: {(data, response, error) -> Void in
+                if let data = data {
+                    
+                    do {
+                        let decoder = JSONDecoder()
+                        let items = try decoder.decode(Item.self, from: data)
+                        completionHandler(items)
+                    } catch {
+                        print("Serialize Error")
+                    }
+                } else {
+                    print(error ?? "Error")
+                }
+            })
+        }
+    }
+    
+    private func getAddConfiguration(url: URL, configuration: URLSessionConfiguration, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let session = URLSession(configuration: configuration)
+        session.dataTask(with: url, completionHandler: completionHandler).resume()
     }
 }
 
@@ -35,12 +83,17 @@ extension ViewController:  UICollectionViewDelegateFlowLayout {
 extension ViewController: UICollectionViewDataSource {
     // セルの数
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return self.items?.hits.count ?? 0
     }
     
     // セルの設定
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pixabayCollectionViewCell", for: indexPath) as! pixabayCollectionViewCell
+        
+        let item = self.items?.hits[indexPath.row]
+        cell.imageCreatorNameLabel.text = item?.user
+        let previewUrl = URL(string: item?.previewURL ?? "")!
+        Nuke.loadImage(with: previewUrl, into: cell.pixabayImageView)
         
         return cell
     }
