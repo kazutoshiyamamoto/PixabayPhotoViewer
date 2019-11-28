a9656f5112929adca50d129a8bb8fdc201352e6d
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-11-28 22:44:23 +0900

    端末によって発生するレイアウト崩れを修正

--- PixabayPhotoViewer/PixabayPhotoViewer/ViewController.swift
+++ PixabayPhotoViewer/PixabayPhotoViewer/ViewController.swift
@@ -142,8 +142,8 @@ extension ViewController:  UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         let numberOfCell: CGFloat = 3
-        let cellWidth = UIScreen.main.bounds.size.width  / numberOfCell - 6
-        return CGSize(width: cellWidth, height: 160)
+        let cellWidth = UIScreen.main.bounds.size.width  / numberOfCell - 2.8
+        return CGSize(width: cellWidth, height: cellWidth + 45)
     }
     
     // フッターの高さ
