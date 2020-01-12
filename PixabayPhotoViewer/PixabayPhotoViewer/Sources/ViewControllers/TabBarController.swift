8b2aa9cb41ad4b594f13613ea35a154ab71557ad
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-01-13 00:55:05 +0900

    TabBarControllerのカスタムクラスを追加

---
+++ PixabayPhotoViewer/PixabayPhotoViewer/Sources/ViewControllers/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  PixabayPhotoViewer
+//
+//  Created by home on 2020/01/13.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
