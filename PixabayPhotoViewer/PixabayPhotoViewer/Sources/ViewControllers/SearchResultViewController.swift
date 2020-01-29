149aa22cf7c3e7ef3fffb03931ffb1377d419c64
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-01-29 22:53:29 +0900

    検索結果画面のカスタムクラスを追加

---
+++ PixabayPhotoViewer/PixabayPhotoViewer/Sources/ViewControllers/SearchResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultViewController.swift
+//  PixabayPhotoViewer
+//
+//  Created by home on 2020/01/29.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
