52eaca5e42a57e9494a8b2d64fa649bb115c94b7
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-02-07 23:32:39 +0900

    JSONファイルの読み込み処理を実装するクラスを追加

---
+++ PixabayPhotoViewer/PixabayPhotoViewer/Sources/Models/JsonFileLoader.swift
@@ -0,0 +1,13 @@
+//
+//  JsonFileLoader.swift
+//  PixabayPhotoViewer
+//
+//  Created by home on 2020/02/07.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+class JsonFileLoader {
+    
+}
