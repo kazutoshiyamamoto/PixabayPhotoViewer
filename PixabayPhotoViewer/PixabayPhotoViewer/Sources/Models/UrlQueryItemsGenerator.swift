8e271cdea094dc72e4d774db316d6c90d03fa71c
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-02-10 23:14:14 +0900

    リクエストURLのクエリを生成する処理を実装するクラスを追加

---
+++ PixabayPhotoViewer/PixabayPhotoViewer/Sources/Models/UrlQueryItemsGenerator.swift
@@ -0,0 +1,13 @@
+//
+//  UrlQueryItemsGenerator.swift
+//  PixabayPhotoViewer
+//
+//  Created by home on 2020/02/10.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+class UrlQueryItemGenerator {
+    
+}
