aa4379737859cbdbd044c416fbb9185eec1ea624
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-02-11 00:15:19 +0900

    APIのURLとAPIキーの定数を追加

---
+++ PixabayPhotoViewer/PixabayPhotoViewer/Sources/Consts/Consts.swift
@@ -0,0 +1,15 @@
+//
+//  Consts.swift
+//  PixabayPhotoViewer
+//
+//  Created by home on 2020/02/10.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+struct Consts {
+    static let apiUrl = "https://pixabay.com/api/"
+    
+    static let apiKey = ""
+}
