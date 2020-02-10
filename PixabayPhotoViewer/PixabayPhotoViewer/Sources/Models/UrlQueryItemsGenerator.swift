38db03352912f1df49675b65d36073326fb24bfd
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-02-10 23:27:32 +0900

    商品一覧取得時のURLQueryItemを生成する処理を追加

--- PixabayPhotoViewer/PixabayPhotoViewer/Sources/Models/UrlQueryItemsGenerator.swift
+++ PixabayPhotoViewer/PixabayPhotoViewer/Sources/Models/UrlQueryItemsGenerator.swift
@@ -8,6 +8,29 @@
 
 import Foundation
 
-class UrlQueryItemGenerator {
-    
+class UrlQueryItemsGenerator {
+    // 商品一覧取得時のURLQueryItemを生成
+    func generateSearchQueryItems(category: String?, page: Int?, perPage: Int?) -> [URLQueryItem] {
+        
+        var namedValues: [(name: String, value: String)] = []
+        
+        if category != nil {
+            let category = (name: "category", value: "\(category!)")
+            namedValues.append(category)
+        }
+        
+        if page != nil {
+            let page = (name: "page", value: "\(page!)")
+            namedValues.append(page)
+        }
+        
+        if perPage != nil {
+            let perPage = (name: "per_page", value: "\(perPage!)")
+            namedValues.append(perPage)
+        }
+        
+        let queryItems = namedValues.map { URLQueryItem(name: $0.name, value: $0.value) }
+        
+        return queryItems
+    }
 }
